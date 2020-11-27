cc03ea0b3568aee273d9f0a4b3aae3bdbf4365e6
Albert Albala <albert@mediatribe.net>
2020-11-27 10:53:58 -0500

    Fix "the input device is not a TTY" for Jenkins job (#2)

--- scripts/test-running-environment.sh
+++ scripts/test-running-environment.sh
@@ -5,13 +5,13 @@
 set -e
 
 echo " => Running self-tests"
-docker-compose exec drupal /bin/bash -c 'drush ev "\Drupal::service('"'"'watchdog_watchdog.self_tester'"'"')->selfTest();"'
+docker-compose exec -T drupal /bin/bash -c 'drush ev "\Drupal::service('"'"'watchdog_watchdog.self_tester'"'"')->selfTest();"'
 
 echo " => Uninstalling watchdog_watchdog_severity"
-docker-compose exec drupal /bin/bash -c 'drush pmu -y watchdog_watchdog_severity'
+docker-compose exec -T drupal /bin/bash -c 'drush pmu -y watchdog_watchdog_severity'
 
 echo " => Uninstalling watchdog_watchdog"
-docker-compose exec drupal /bin/bash -c 'drush pmu -y watchdog_watchdog'
+docker-compose exec -T drupal /bin/bash -c 'drush pmu -y watchdog_watchdog'
 
 echo " => Done running self-tests. All WWatchdog modules should be uninstalled."
 echo " =>"
